1d9a997e33bf28935b274d7ee70e9a878b3ac9be
Sargun Vohra <sargunv@users.noreply.github.com>
2026-06-27 14:29:46 -0700

    refine

--- cmake/mln_install.cmake
+++ cmake/mln_install.cmake
@@ -12,6 +12,13 @@ function(mln_install_c_api_library target)
     LIBRARY DESTINATION "${CMAKE_INSTALL_LIBDIR}"
     ARCHIVE DESTINATION "${CMAKE_INSTALL_LIBDIR}")
 
+  if(MSVC)
+    install(
+      FILES "$<TARGET_PDB_FILE:${target}>"
+      DESTINATION "${CMAKE_INSTALL_BINDIR}"
+      OPTIONAL)
+  endif()
+
   install(
     FILES "${PROJECT_SOURCE_DIR}/include/maplibre_native_c.h"
     DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}")
